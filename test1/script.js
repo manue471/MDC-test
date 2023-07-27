248459251fc99468f941bd469251b4d323710255
Pablo Emanuel <maneu471@gmail.com>
2023-07-27 20:55:26 -0300

    fix(lint)

--- test1/script.js
+++ test1/script.js
@@ -1,9 +1,9 @@
-document.getElementById('change-text-btn').onclick = function() {
+document.getElementById('change-text-btn').onclick = function () {
   changeTextValue('text', 'Thanks for the oportunity...');
 }
 
-const changeTextValue = (text, value) => { 
-  const showedText ='Hello World!!';
+const changeTextValue = (text, value) => {
+  const showedText = 'Hello World!!';
   const actualValue = document.getElementById(text).textContent;
   const newValue = value == actualValue ? showedText : value;
   document.getElementById(text).textContent = newValue;
