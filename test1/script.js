2edd117160dc276b235fc3131d1f20f1d9d0f06e
Pablo Emanuel <maneu471@gmail.com>
2023-07-27 20:43:44 -0300

    feat(test1): completed

---
+++ test1/script.js
@@ -0,0 +1,10 @@
+document.getElementById('change-text-btn').onclick = function() {
+  changeTextValue('text', 'Thanks for the oportunity...');
+}
+
+const changeTextValue = (text, value) => { 
+  const showedText ='Hello World!!';
+  const actualValue = document.getElementById(text).textContent;
+  const newValue = value == actualValue ? showedText : value;
+  document.getElementById(text).textContent = newValue;
+}
